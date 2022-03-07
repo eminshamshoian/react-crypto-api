b25926a85ffa6ed4d8cd9a69c19f6e5f8ee0e6b3
Emin <eminshamshoian@gmail.com>
2022-03-07 00:10:51 -0800

    created firebase account and linked it to app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import { Routes, Route } from 'react-router-dom'
-import Coins from './components/Coins'
-import Coin from './routes/Coin'
-import Navbar from './components/Navbar'
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Routes, Route } from "react-router-dom";
+import Coins from "./components/Coins";
+import Coin from "./routes/Coin";
+import Navbar from "./components/Navbar";
 
 function App() {
+  const [coins, setCoins] = useState([]);
 
-  const [coins, setCoins] = useState([])
-
-  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false'
+  const url =
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false";
 
   useEffect(() => {
-    axios.get(url).then((response) => {
-      setCoins(response.data)
-      // console.log(response.data[0])
-    }).catch((error) => {
-      console.log(error)
-    })
-  }, [])
+    axios
+      .get(url)
+      .then((response) => {
+        setCoins(response.data);
+        // console.log(response.data[0])
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   return (
     <>
@@ -30,7 +32,6 @@ function App() {
           <Route path=':coinId' element={<Coin />} />
         </Route>
       </Routes>
-
     </>
   );
 }
