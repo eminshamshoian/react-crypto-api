0cf01d1134ba439550304fc5e578204ea35cf8d6
tristanepang <82466679+tristanepang@users.noreply.github.com>
2022-03-09 17:36:23 -0800

    Update Signup.js

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,5 +1,5 @@
-import React, { useRef } from "react"
-import { Form, Button, Card } from "react-bootstrap"
+import React, { useRef, useState } from "react"
+import { Form, Button, Card, Alert } from "react-bootstrap"
 import { Container } from "react-bootstrap"
 import { useAuth } from '../contexts/AuthContext'
 
@@ -8,11 +8,25 @@ export default function Signup() {
   const passwordRef = useRef()
   const passwordConfirmRef = useRef()
   const { signup } = useAuth()
+  const [error, setError] = useState("")
+  const [loading, setLoading] = useState(false)
 
-  function handleSubmit(e) {
-    e.preventDefautl()
+  async function handleSubmit(e) {
+    e.preventDefault()
 
-    signup(emailRef.current.value, passwordRef.current.value)
+    if (passwordRef.current.value !== 
+    passwordConfirmRef.current.value) {
+      return setError("Passwords do not match")
+    }
+
+    try{
+      setError("")
+      setLoading(true)
+      await signup(emailRef.current.value, passwordRef.current.value)
+    } catch{
+      setError("Failed to create an account")
+    }
+    setLoading(false)
   }
 
   return (
@@ -25,8 +39,9 @@ export default function Signup() {
           <Card>
             <Card.Body>
               <h2 className="text-center mb-4">Sign Up</h2>
+              {error && <Alert variant="danger">{error}</Alert>}
             </Card.Body>
-            <Form>
+            <Form onSubmit={handleSubmit}>
               <Form.Group id="email">
                 <Form.Label>Email</Form.Label>
                 <Form.Control type="email" ref={emailRef} required />
@@ -43,7 +58,7 @@ export default function Signup() {
                   required
                 />
               </Form.Group>
-              <Button className="w-100" type="submit">
+              <Button disabled={loading} className="w-100" type="submit">
                 Sign Up
               </Button>
             </Form>
